725394d51d28046139a6f444cc5f8deba2eb8f0c
ChiLi_Lin <904981663@163.com>
2017-09-28 17:42:29 +0800

    Draw Scale Handle

--- Assets/Editor/PointAnnulusAreaEditor.cs
+++ Assets/Editor/PointAnnulusAreaEditor.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 [CustomEditor(typeof(PointAnnulusArea))]
 public class PointAnnulusAreaEditor : Editor
 {
     private PointAnnulusArea Target;
+    private float scaleValue;
+    private Matrix4x4 mOld;
+    private Color colorOld;
 
     private void OnEnable()
     {
@@ -28,16 +32,19 @@ public class PointAnnulusAreaEditor : Editor
 
     private void OnSceneGUI()
     {
-        Matrix4x4 mOld = Handles.matrix;
-        Color colorOld = Handles.color;
+        mOld = Handles.matrix;
+        colorOld = Handles.color;
 
         DrawAnnulusArea();
-        DrawScaleHandle();
+        DrawMinMaxScaleHandle();
 
         Handles.color = colorOld;
         Handles.matrix = mOld;
     }
 
+    /// <summary>
+    /// 绘制环形区域
+    /// </summary>
     private void DrawAnnulusArea()
     {
         Handles.color = Target.appearance.outerCircleColor;
@@ -46,10 +53,40 @@ public class PointAnnulusAreaEditor : Editor
         Handles.DrawSolidDisc(Target.transform.position, Target.transform.up, Target.MinRadius);
     }
 
-    private void DrawScaleHandle()
+    /// <summary>
+    /// 绘制内外缩放控制
+    /// </summary>
+    private void DrawMinMaxScaleHandle()
     {
-        Handles.color = Target.appearance.edgeColor;
-        Handles.CircleHandleCap(0, Target.transform.position, Quaternion.LookRotation(Target.transform.up), Target.MinRadius, EventType.Repaint);
-        Handles.CircleHandleCap(0, Target.transform.position, Quaternion.LookRotation(Target.transform.up), Target.MaxRadius, EventType.Repaint);
+        scaleValue = DrawScaleHandle(Target.MinRadius, Target.appearance.innerEdgeColor, Target.transform.right);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Scale Min Radius");
+            Target.MinRadius = scaleValue;
+            Repaint();
+        }
+
+        scaleValue = DrawScaleHandle(Target.MaxRadius, Target.appearance.outerEdgeColor, Target.transform.forward);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Scale Max Radius");
+            Target.MaxRadius = scaleValue;
+            Repaint();
+        }
+    }
+
+    /// <summary>
+    /// 绘制缩放控制
+    /// </summary>
+    /// <param name="radius">半径</param>
+    /// <param name="color">颜色</param>
+    /// <param name="direction">方向</param>
+    /// <returns>返回缩放值</returns>
+    private float DrawScaleHandle(float radius,Color color,Vector3 direction)
+    {
+        Handles.color = color;
+        Handles.CircleHandleCap(0, Target.transform.position, Quaternion.LookRotation(Target.transform.up), radius, EventType.Repaint);
+        EditorGUI.BeginChangeCheck();
+        return Handles.ScaleSlider(radius, Target.transform.position, direction, Target.transform.rotation, HandleUtility.GetHandleSize(Target.transform.position), 0.5f);
     }
 }
---
+++ Assets/Examples/Scirpts/SpawnFromAnnulusArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFromAnnulusArea : MonoBehaviour 
+{
+    public PointAnnulusArea area;
+    public GameObject prefab;
+    public int amount = 10;
+
+    private List<GameObject> objList = new List<GameObject>();
+
+    private void Start()
+    {
+        Point p;
+        for (int i = 0; i < amount; i++)
+        {
+            objList.Add(Instantiate(prefab));
+            //p = area.GetRandomPointInArea()/*.GetWorldSpacePoint(transform)*/;
+            objList[i].transform.position = area.GetRandomPositionInArea();
+            //objList[i].transform.rotation = p.rotation;
+            //Debug.Log(p.position + "  " + p.eulerAngles);
+        }
+    }
+
+
+}
